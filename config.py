86e6a8006d2865d4f971a40a3f0f8cbe29184e0f
Cevilion <sumitsajwan135@gmail.com>
2025-03-19 15:25:05 +0530

    Update config.py

--- config.py
+++ config.py
@@ -2,7 +2,7 @@
 
 API_ID = "22403100" #API ID get it from telegram.org
 API_HASH = "ccbc3f662735abfa604ef6309ba76e67" #API HASH get it from telegram.org
-BOT_TOKEN = "7712645195:AAHZRa3z3IJ_Zty-pTW_-XqngdSIcdTpKjo" #Bot Token get it from @BotFather
+BOT_TOKEN = "7810884152:AAGBGkYW-DlONSaIozRUbI4N9o3Uri7jCOc" #Bot Token get it from @BotFather
 START_PIC = "https://imgur.com/a/aZBWfUL" #Startup Pic
 MONGO_URI = "mongodb+srv://sumitsajwan135:gameno01@cluster0.ja0i0.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0" #Databse uri get it from https://www.mongodb.com/
 STICKER_ID = "CAACAgUAAxkBAAEOHZJn2oDMces0VpmDLtLk1lIxOqWQCQACkxQAApZn0FY1op6f8Hu6hDYE" # Replace with your sticker's file ID
