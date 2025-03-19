32ab80a8885ad9d7728aede7acb94e589402db04
Cevilion <sumitsajwan135@gmail.com>
2025-03-19 12:42:11 +0530

    Update config.py

--- config.py
+++ config.py
@@ -3,7 +3,7 @@
 API_ID = "22403100" #API ID get it from telegram.org
 API_HASH = "ccbc3f662735abfa604ef6309ba76e67" #API HASH get it from telegram.org
 BOT_TOKEN = "7712645195:AAHZRa3z3IJ_Zty-pTW_-XqngdSIcdTpKjo" #Bot Token get it from @BotFather
-URL_A = "https://www.animenewsnetwork.com/news/rss.xml" #News RSS Feed Url 2
-URL_B = "https://www.theanimedaily.com/feed" #News RSS Feed Url 2
+URL_A = "https://www.crunchyroll.com/news/rss" #News RSS Feed Url 2
+URL_B = "https://myanimelist.net/rss/news.xml" #News RSS Feed Url 2
 START_PIC = "https://imgur.com/a/aZBWfUL" #Startup Pic
 MONGO_URI = "mongodb+srv://sumitsajwan135:gameno01@cluster0.ja0i0.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0" #Databse uri get it from https://www.mongodb.com/
