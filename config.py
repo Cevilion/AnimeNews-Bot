29aba4b6f04e30f640fb0f8f4008f5487d6535fd
Cevilion <sumitsajwan135@gmail.com>
2025-03-21 08:12:26 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,11 +1,11 @@
 # config.py
 
-API_ID = "22403100" #API ID get it from telegram.org
-API_HASH = "ccbc3f662735abfa604ef6309ba76e67" #API HASH get it from telegram.org
-BOT_TOKEN = "7810884152:AAGBGkYW-DlONSaIozRUbI4N9o3Uri7jCOc" #Bot Token get it from @BotFather
-START_PIC = "https://imgur.com/a/aZBWfUL" #Startup Pic
-MONGO_URI = "mongodb+srv://sumitsajwan135:gameno01@cluster0.ja0i0.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0" #Databse uri get it from https://www.mongodb.com/
-STICKER_ID = "CAACAgUAAxkBAAEOHZJn2oDMces0VpmDLtLk1lIxOqWQCQACkxQAApZn0FY1op6f8Hu6hDYE" # Replace with your sticker's file ID
+API_ID = "" #API ID get it from telegram.org
+API_HASH = "" #API HASH get it from telegram.org
+BOT_TOKEN = "" #Bot Token get it from @BotFather
+START_PIC = "" #Startup Pic
+MONGO_URI = "" #Databse uri get it from https://www.mongodb.com/
+STICKER_ID = "" # Replace with your sticker's file ID
 NEWS_FEED_URLS = [
     "https://cr-news-api-service.prd.crunchyrollsvc.com/v1/en-US/rss",
     "https://myanimelist.net/rss/news.xml"
